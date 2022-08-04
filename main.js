fdfeb0f9902963ee146f0667fe59b189036b43cb
Kidkidlo54 <95107330+Kidkidlo54@users.noreply.github.com>
2022-08-04 16:51:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+function setup(){
+    canvas=createCanvas(500,500)
+    canvas.position(128,256)
+    video=createCapture(VIDEO)
+    video.hide()
+    filter=""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function draw(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    image(video,0,0,500,500)
+    tint(filter)
+    
+}
+
+function Class(){
+    filter=document.getElementById("gavin").value
+}
+
+function albertfinction(){
+    save("Function free money(){copy this link for free money https://www.youtube.com/watch?v=dQw4w9WgXcQ}.png")
+}
